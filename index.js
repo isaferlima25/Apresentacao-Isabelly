3be1bc4cb6e2b9acab28cb2a61caa5ab7c1f02f5
isaferlima25 <167373738+isaferlima25@users.noreply.github.com>
2024-08-29 01:27:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const primeiroNome = "Isabelly";
+const sobrenome = "Fernandes Lima"
+const idade = 17;
+const altura = 1.72;
+const cidadeNatal = "Curitiba";
+const massa = 64
+
+console.log("O meu primeiro nome é:")
+console.log(primeiroNome);
+console.log("O meu sobrenome é:")
+console.log(sobrenome);
+console.log("A minha idade é:")
+console.log(idade);
+console.log("A minha altura é:")
+console.log(altura);
+console.log("A minha cidade natal é:")
+console.log(cidadeNatal);
+console.log("Minha massa é:")
+console.log(massa);
